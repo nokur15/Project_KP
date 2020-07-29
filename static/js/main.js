87f15dd775705380ede807ae5963437b2cdd611d
Naufal Kurniawan <naufal.kurniawan.indonesia.111@gmail.com>
2020-07-29 12:22:37 +0700

    untuk html kakaknya

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+  if (!navigator.serviceWorker) return;
+
+  navigator.serviceWorker.register('/sw.js')
+    .then(function(reg) {
+      console.info('SW is registered.', reg);
+    })
+    .catch(function() {
+      console.error('Failed to register SW.');
+    });
+}
